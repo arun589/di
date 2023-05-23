d4b44a88ca1104be5f6f08655980b4c245a08578
arun589 <83627958+arun589@users.noreply.github.com>
2023-05-23 14:13:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 
 var randomNumber1=Math.random()*6+1;
 randomNumber1=Math.floor(randomNumber1);
-var randomImage="images/dice"+randomNumber1+".png";
+var randomImage="dice"+randomNumber1+".png";
 document.querySelectorAll("img")[0].setAttribute("src",randomImage);
 var randomNumber2=Math.floor(Math.random()*6+1);
-var randomImage2="images/dice"+randomNumber2+".png";
+var randomImage2="dice"+randomNumber2+".png";
 document.querySelector(".img2").setAttribute("src",randomImage2);
 if(randomNumber1>randomNumber2)
 {
